61bb1df32ffbe04969dabaab40fc47998b4694aa
Sapeiidw <sapeiidw@gmail.com>
2021-09-08 20:49:24 +0800

    edit switcher

--- resources/views/components/switcher.blade.php
+++ resources/views/components/switcher.blade.php
@@ -1,12 +1,9 @@
-{{-- <span class="text-gray-700 dark:text-gray-100">light</span> --}}
 <div @click="darkMode = !darkMode"
-    class="relative flex flex-column items-center w-9 h-5 mx-2 pl-0 rounded-full bg-gray-300 cursor-pointer">
-    {{-- <i class="fas fa-sun"></i> --}}
-    {{-- <i class="fas fa-moon"></i> --}}
+    class="relative flex flex-column items-center w-9 h-5 mx-2 pl-0 rounded-full bg-gray-300 dark:bg-gray-600 cursor-pointer">
         <span
-        class="block fas fa-moon border border-gray-300 rounded-full items-center appearance-none cursor-pointer outline-none transition-all ease-in-out duration-500"
+        class="block fas rounded-full items-center appearance-none cursor-pointer outline-none transition-all ease-in-out duration-300"
         x-init="$watch('darkMode', val => localStorage.setItem('dark', val))"
-        x-bind:class="{ 'ml-4': darkMode }">
+        x-bind:class="darkMode ? 'fa-moon text-white ml-4' : 'fa-sun text-yellow-500' ">
         </span>
 </div>
-{{-- <span class="text-gray-700 dark:text-gray-100 mr-2">dark</span> --}}
+
