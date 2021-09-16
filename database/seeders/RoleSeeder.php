bc44303a24cc080b4fb0aa5d396517b8d4a1b8dd
Sapeiidw <sapeiidw@gmail.com>
2021-09-16 17:49:27 +0800

    role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -32,7 +32,6 @@ class RoleSeeder extends Seeder
         $role1 = Role::find(1);
         $role1->syncPermissions();
         $role2 = Role::find(2);
-        $role2->permissions()->sync([2,6,10,14,18]);
+        $role2->permissions()->sync([2,6,10,14,17,18]);
     }
-    // 13,14,15,16,17,18,19,20,21,22,23,24
 }
