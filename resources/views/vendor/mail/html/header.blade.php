83a0f80961fed0fcb01f828b09e645f5765929e6
niCknaMe134 <11181067@student.itk.ac.id>
2021-09-21 21:04:31 +0800

    logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'SSO ITK')
-<img src="{{ asset('/img/SSOITK.png') }}" class="logo" alt="SSO ITK">
+<img src="http://sso.itk.ac.id/img/SSOITK.png" class="logo" alt="SSO ITK">
 @else
 {{ $slot }}
 @endif
