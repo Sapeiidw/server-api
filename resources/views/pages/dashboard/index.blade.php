4822f59820eae9e18a9fa9dd679d60d927d154e7
niCknaMe134 <11181067@student.itk.ac.id>
2021-09-21 18:18:07 +0800

    admin

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -2,8 +2,8 @@
     @section('title', 'Admin')
     <div class="w-4/5">
         <div class="flex min-w-screen h-1/6 m-2 bg-white border border-gray-200 shadow rounded-2xl items-center justify-center">
-            <div class=" justify-center items-center font-medium text-3xl text-gray-700">
-                Selamat Datang Di AdminPanel
+            <div class=" justify-center items-center font-medium sm:text-3xl text-lg text-gray-700 px-4 py-2">
+                Selamat Datang Di Panel Admin
             </div>
         </div>
     </div>
