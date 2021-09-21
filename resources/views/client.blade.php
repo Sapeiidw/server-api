739323fcccb7118f58f18d5bb5db267a8939306a
niCknaMe134 <11181067@student.itk.ac.id>
2021-09-21 17:38:16 +0800

    responsive

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -15,7 +15,7 @@
                 @else
                     <div class="px-4 pt-4 pb-3 m-4 dark:bg-gray-800 bg-white shadow rounded-xl">
                         <a href="{{ $item->url }}" class="text-center">
-                            <img src="{{ asset('storage/'.$item->thumbnail) }}" alt="{{ $item->name }}" class="flex sm:w-56 sm:h-28 lg:w-96 lg:h-64 w-36 h-28 min-w-full items-center justify-center dark:bg-gray-700 bg-gray-100">
+                            <img src="{{ asset('storage/'.$item->thumbnail) }}" alt="{{ $item->name }}" class="flex sm:w-56 sm:h-28 lg:w-96 lg:h-64 w-72 h-44 min-w-full items-center justify-center dark:bg-gray-700 bg-gray-100">
                             <div class="flex items-center justify-center mt-2">
                                 <h1>{{ $item->name }}</h1>
                             </div>
--- resources/views/components/boxtable.blade.php
+++ resources/views/components/boxtable.blade.php
@@ -1,6 +1,6 @@
 <div class=" relative w-full flex">
-    <div class="sm:p-2 w-full">
-        <div class="dark:bg-gray-800 dark:border-gray-700 sm:shadow sm:border border-gray-200 sm:rounded-2xl bg-white sm:p-2">
+   <div class="sm:p-2 w-full">
+        <div class="dark:bg-gray-800 dark:border-gray-700 sm:shadow sm:border border-gray-200 sm:rounded-2xl bg-white sm:px-2 py-2">
             <div class="dark:bg-gray-800 py-1 px-2 bg-gray-100 overflow-hidden rounded-xl">
                 <div class="px-4 pt-4">
                     <x-alert></x-alert>
