7dde3d4cf84d8b21530551149b9bf95fb2fe386d
niCknaMe134 <11181067@student.itk.ac.id>
2021-09-21 21:51:56 +0800

    button

--- resources/views/vendor/jetstream/components/button.blade.php
+++ resources/views/vendor/jetstream/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center px-4 py-2 my-1 bg-blue-500 border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'flex-none inline-flex items-center justify-center px-4 py-2 my-1 bg-blue-500 border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-300 disabled:opacity-25 transition']) }}>
     {{ $slot }}
 </button>
